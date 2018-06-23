7568c4ffdb2a465faf3aeae27d14049fd6f472df
brittanywang <35832643+brittanywang@users.noreply.github.com>
2018-06-23 00:39:37 -0700

    updated firebase.js
    included facebook auth func

--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,7 @@ const config = {
 firebase.initializeApp(config);
 
 //exports the auth module of Firebase + Google auth provider  
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 export default firebase;
